2797314facfe36619befa5d92a0e3acad5ce2d2c
stepanstrepetov0 <stepanstrepetov0@gmail.com>
2025-05-15 20:33:29 +0000

    fdr

--- backend/app/routes/main_routes.py
+++ backend/app/routes/main_routes.py
@@ -1,5 +1,11 @@
-from flask import jsonify
+from flask import jsonify, send_file
 from . import api_bp
+import os
+
+@api_bp.route('/', methods=['GET'])
+def serve_frontend():
+    """Serves the frontend index.html file."""
+    return send_file(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'frontend', 'index.html')))
 
 @api_bp.route('/health', methods=['GET'])
 def health_check():
